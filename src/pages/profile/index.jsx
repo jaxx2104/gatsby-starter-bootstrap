96bd77e9cbbec3bb820089d8d82d648806ed546a
Futoshi Iwashita <jaxx2104@gmail.com>
2017-11-27 01:02:18 +0900

    Add Plugins (#24)
    * Update Manifest
    
    * Update modules

--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -1,12 +1,6 @@
 import React from 'react'
 
 class Profile extends React.Component {
-  componentDidMount() {
-    if (typeof twttr != 'undefined') {
-      twttr.widgets.load(this.refs.twButton)
-    }
-  }
-
   render() {
     const pathPrefix =
       process.env.NODE_ENV === 'development' ? '' : __PATH_PREFIX__
