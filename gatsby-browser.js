5011659d9d5ba72a3f54988708091220a932aa03
jaxx2104 <jaxx2104@gmail.com>
2020-06-04 11:08:08 +0900

    :sparkles: Add jquery

---
+++ gatsby-browser.js
@@ -0,0 +1,6 @@
+// Import all js dependencies.
+import 'jquery/dist/jquery.min.js'
+import 'popper.js/dist/popper.min'
+import 'bootstrap/js/dist/util'
+import 'bootstrap/js/dist/carousel'
+import 'bootstrap/js/dist/dropdown'
