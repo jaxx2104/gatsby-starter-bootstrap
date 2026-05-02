7c783260b1773724f985a812b33a638a4ebdee32
jaxx2104 <jaxx2104@gmail.com>
2026-05-03 02:09:03 +0900

    refactor(layout): use useSiteMetadata, drop emergence/animate/font-awesome CSS

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,32 +1,26 @@
-import React, { useEffect } from 'react'
-import emergence from 'emergence.js'
+import React from 'react'
 
 import Navibar from '../navibar/navibar'
 import Footer from '../footer/footer'
-import { siteMetadata } from '../../../gatsby-config'
+import { useSiteMetadata } from '../../hooks/useSiteMetadata'
 
 import 'modern-normalize/modern-normalize.css'
 import 'prismjs/themes/prism.css'
 import 'scss/gatstrap.scss'
-import 'animate.css/animate.css'
-import 'font-awesome/css/font-awesome.css'
 
 interface Props {
   children?: React.ReactNode
   location: Location
 }
 
-const Layout: React.FC<Props> = ({ children, location }: Props) => {
-  useEffect(() => {
-    emergence.init()
-  })
-
+const Layout: React.FC<Props> = ({ children, location }) => {
+  const meta = useSiteMetadata()
   return (
-    <div>
-      <Navibar title={siteMetadata.title} location={location} />
-      {children}
-      <Footer title={siteMetadata.title} author={siteMetadata.author} />
-    </div>
+    <>
+      <Navibar title={meta.title} location={location} />
+      <main>{children}</main>
+      <Footer title={meta.title} author={meta.author} />
+    </>
   )
 }
 
