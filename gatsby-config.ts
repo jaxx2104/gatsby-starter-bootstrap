f392aeb6b4bb1753fb8da670a7d167c1fdcc01d3
jaxx2104 <jaxx2104@gmail.com>
2026-05-03 02:06:53 +0900

    refactor(config): port gatsby-config to TypeScript with consolidated siteMetadata

---
+++ gatsby-config.ts
@@ -0,0 +1,73 @@
+import type { GatsbyConfig } from 'gatsby'
+
+const siteMetadata = {
+  title: 'Gatstrap',
+  description: 'A Bootstrap 5 starter for Gatsby',
+  siteUrl: 'https://gatstrap.netlify.app',
+  author: 'jaxx2104',
+  twitter: 'jaxx2104',
+} as const
+
+const config: GatsbyConfig = {
+  siteMetadata,
+  graphqlTypegen: {
+    typesOutputPath: 'src/gatsby-types.d.ts',
+  },
+  plugins: [
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: { path: `${__dirname}/content/posts/`, name: 'posts' },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: { path: `${__dirname}/content/images/`, name: 'images' },
+    },
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              maxWidth: 750,
+              linkImagesToOriginal: false,
+              wrapperStyle: 'margin-bottom: 1.0725rem;',
+            },
+          },
+          {
+            resolve: 'gatsby-remark-responsive-iframe',
+            options: { wrapperStyle: 'margin-bottom: 1.0725rem' },
+          },
+          'gatsby-remark-prismjs',
+          'gatsby-remark-copy-linked-files',
+          'gatsby-remark-smartypants',
+        ],
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: siteMetadata.title,
+        short_name: siteMetadata.title,
+        description: siteMetadata.description,
+        start_url: '/',
+        background_color: '#ffffff',
+        theme_color: '#673ab7',
+        display: 'standalone',
+        icon: 'static/og-image.png',
+      },
+    },
+    'gatsby-plugin-catch-links',
+    'gatsby-plugin-image',
+    'gatsby-plugin-netlify',
+    'gatsby-plugin-offline',
+    'gatsby-plugin-sass',
+    'gatsby-plugin-sharp',
+    'gatsby-plugin-sitemap',
+    'gatsby-plugin-twitter',
+    'gatsby-transformer-sharp',
+  ],
+}
+
+export default config
+export { siteMetadata }
