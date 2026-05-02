9e608600fcd8e698fe3ed35790837192494c3d19
jaxx2104 <jaxx2104@gmail.com>
2026-05-03 02:09:27 +0900

    chore(lint): migrate to ESLint 9 flat config and Prettier 3

---
+++ eslint.config.mjs
@@ -0,0 +1,38 @@
+import js from '@eslint/js'
+import tseslint from '@typescript-eslint/eslint-plugin'
+import tsParser from '@typescript-eslint/parser'
+import react from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import prettier from 'eslint-plugin-prettier'
+import prettierConfig from 'eslint-config-prettier'
+import globals from 'globals'
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: { ecmaFeatures: { jsx: true } },
+      globals: { ...globals.browser, ...globals.node },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+      react,
+      'react-hooks': reactHooks,
+      prettier,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      ...prettierConfig.rules,
+      'react/react-in-jsx-scope': 'off',
+      'prettier/prettier': 'error',
+    },
+    settings: { react: { version: '19.0.0' } },
+  },
+  {
+    ignores: ['public/**', '.cache/**', 'src/__generated__/**', 'node_modules/**'],
+  },
+]
